435aa5af3faf8fc6e39b10b7491e94b2ee580e46
Devang Bacharwar <devanggtgr8@gmail.com>
2018-06-04 17:14:25 +0530

    Remove secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Donate::Application.config.secret_key_base = 'b170dfb4d5719323cc8a77f1d4729f57c8d36ccafd9e678a3a4d2e2745ec0381857fe29799d0e4cfcb64d3467f0ee833d6f742694ea76e3933f3ae0972625231'
+Donate::Application.config.secret_key_base = '############'
